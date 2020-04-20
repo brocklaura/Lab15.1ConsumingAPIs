e891ad3a3c7441564b8fc52f198c9d32ec3c9d3a
Laura Brock <laurabrock@quickenloans.com>
2020-04-20 19:51:38 -0400

    first commit

---
+++ Lab15.1ConsumingAPIs/Controllers/HomeController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Lab15._1ConsumingAPIs.Models;
+using System.Net.Http;
+
+namespace Lab15._1ConsumingAPIs.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public async Task<IActionResult> DrawCards()
+        {
+            var client = new HttpClient();
+            client.BaseAddress = new Uri("http://deckofcardsapi.com/");
+            client.DefaultRequestHeaders.UserAgent.ParseAdd("Mozilla/5.0 (compatible; GrandCircus/1.0)");
+
+
+            var response = await client.GetAsync("api/deck/new/draw/?count=5");
+
+            var deal = await response.Content.ReadAsAsync<Deal>();
+
+            return View(deal);
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Lab15.1ConsumingAPIs/Models/Deal.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lab15._1ConsumingAPIs.Models
+{
+    public class Deal
+    {
+        public string deckID { get; set; }
+        public Card[] Cards { get; set; }
+
+    }
+
+    public class Card
+    {
+        public Uri Image { get; set; }
+        public string Value { get; set; }
+        public string Suit { get; set; }
+    }
+}
